ef680ff4c20decae5f8b5d980e4ecd80a2704303
Arthur Verschaeve <contact@arthurverschaeve.be>
2014-06-28 17:23:12 +0200

    Added better basic js

--- index.php
+++ index.php
@@ -1,25 +1,25 @@
 <!doctype html>
 <html class="no-js" lang="">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>Boilerplate</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Boilerplate</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
+    <!-- Place favicon.ico and apple-touch-icon(s) in the root directory -->
 
-        <link rel="stylesheet" href="css/main.css">
-    </head>
-    <body>
-        <!--[if lt IE 8]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-
-            
-
-        <script src="//localhost:35729/livereload.js"></script>    
-        <script src="js/vendor/vendor.js"></script>
-        <script src="js/main.min.js"></script>
-    </body>
+    <link href="css/main.css" rel="stylesheet">
+  </head>
+  <body>
+    <!--[if lt IE 8]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+  
+                
+  
+    <script src="//localhost:35729/livereload.js"></script>    
+    <script src="js/vendor/vendor.js"></script>
+    <script src="js/main.min.js"></script>
+  </body>
 </html>
