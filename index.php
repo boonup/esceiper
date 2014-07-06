9905a653961b35518b3cec6550a39e71d11618f1
Arthur Verschaeve <contact@arthurverschaeve.be>
2014-07-06 21:38:55 +0200

    Add markup

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Boilerplate</title>
+    <title>Esceiper</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -16,7 +16,24 @@
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
   
-                
+    	<div class='container'>
+    		<h1>Esceiper</h1>
+    		<p class='intro'>
+	    		Escaping HTML the right way is always kinda tricky. You can't remember that one specific character, or you just want a machine to put in your text and does the escaping for you. Use <strong>Esceiper</strong>
+    		</p>
+    		
+    		<div class='app'>
+    			<form method="post" action='#'>
+    				<textarea name="input"></textarea>
+    				<button type='submit'> Escape! </button>
+    			</form>
+    			<textarea class='result'></textarea>
+    		</div>
+    		
+    		<div class='info'>
+    			<p>Hope you're enjoying this tool. You can contribute on <a href="#">Github</a></p>
+    		</div>
+    	</div>
   
     <script src="//localhost:35729/livereload.js"></script>    
     <script src="js/vendor/vendor.js"></script>
