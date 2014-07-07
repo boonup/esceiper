26fdb686e739302f4692202809dcf46613ac02ec
Arthur Verschaeve <contact@arthurverschaeve.be>
2014-07-07 08:43:27 +0200

    Removed some not-used frameworks and libraries.

--- js/vendor/v_on.js
+++ js/vendor/v_on.js
@@ -1,10 +1,3 @@
-
-if (!String.prototype.trim) {
-  String.prototype.trim = function () {
-    return this.replace(/^\s+|\s+$/g, '');
-  };
-}
-
 function on( element, type, handler ) {
   if (element.addEventListener) element.addEventListener(type, handler, false);
   else if (element.attachEvent) element.attachEvent('on' + type, handler);
